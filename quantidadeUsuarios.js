0347614b1524cb63f3df76747ef571a6c3026c5e
joaodelas6901 <161736695+joaodelas6901@users.noreply.github.com>
2024-10-24 11:35:56 -0300

    fsdfghghdfg

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./common.js"
+import {getCSS, tickConfig} from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -21,6 +21,37 @@ async function quantidadeUsuariosPorRede() {
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color')
+       
+        title: {
+            text: 'Redes sociais com mais usuários',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                size: 30,
+                font: getCSS('--font')
+            }
+        },
+
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Nome das redes',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'Bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    }
+
 
     }
 
@@ -28,6 +59,6 @@ async function quantidadeUsuariosPorRede() {
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico, data, layout)
-}
+
 
 quantidadeUsuariosPorRede()
